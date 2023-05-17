2ee06f0975c6383ba4ce29c8bb33173e81d8b3d3
HajdukSanchez <hajduksanchez.dev@gmail.com>
2023-05-17 08:05:05 -0500

    🎉 [BEGIN] New project
    Start Bom Hamburguer project in Flutter

---
+++ android/app/src/main/kotlin/com/hajduksanchez/bomhamburguer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hajduksanchez.bomhamburguer.bom_hamburguer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:bom_hamburguer/app/app.dart';
+import 'package:bom_hamburguer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
